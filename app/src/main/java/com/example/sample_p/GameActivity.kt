7bd9f446b40d1d8a89a8e5ff30e8ba0bcd6f56cd
Snovv <sho520coa@gmail.com>
2021-07-07 14:21:54 +0900

    Back Button Active

--- app/src/main/java/com/example/sample_p/GameActivity.kt
+++ app/src/main/java/com/example/sample_p/GameActivity.kt
@@ -1,11 +1,19 @@
 package com.example.sample_p
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+    }
+
+    fun button3Tapped(view: View?){
+        val intent= Intent(this,MainActivity::class.java)
+        startActivity(intent)
     }
 }
