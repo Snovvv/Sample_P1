ba9036ca04bb76734f859e43dc85aa08f99b1f7c
gotodaiki <bigtree0143@icloud.com>
2021-07-07 13:53:47 +0900

    tobuzo

--- app/src/main/java/com/example/sample_p/MainActivity.kt
+++ app/src/main/java/com/example/sample_p/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.sample_p
 
-import android.os.Bundle
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun button3Tapped(view: View?){
+        val intent= Intent(this,GameActivity::class.java)
+        startActivity(intent)
     }
 }
